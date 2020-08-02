be4ecdf363803f0d99d8cde8379741fd97ea0f9d
Nicholas Aelick <naelick@espial.com>
2020-08-01 22:25:35 -0400

    Break sample code into files, automate build/deploy, enable oauth

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,67 @@
+/* global module: false */
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      files: ['Gruntfile.js', 'lib/pen-master/src/**/*.js', 'src/**/*.js']
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'build/autowiki-<%= pkg.version %>.min.js': ['lib/pen-master/src/**/*.js', 'src/*.js']
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+
+    copy: {
+      build: {
+        files: [
+          {expand: true, flatten:true, src: ['lib/pen-master/src/font/*'], dest: 'build/font/', filter: 'isFile'},
+          {expand: true, flatten:true, src: ['lib/pen-master/src/*.css'], dest: 'build/', filter: 'isFile'},
+          {expand: true, flatten:true, src: ['src/*.css'], dest: 'build/', filter: 'isFile'},
+          {expand: false, src: ['index.html', '404.html'], dest: 'build/', filter: 'isFile'},
+        ]
+      }
+    },
+    gcs: {
+      options: {
+        keyFilename: 'keyfile.json',
+        project: 'natural-broker-285114',
+        bucket: 'autowiki.naelick.com',
+        gzip: false,
+        headers: {
+        },
+        metadata: {
+        }
+      },
+      dist: {
+        cwd: 'build',
+        src: '**'
+      },
+    }
+  });
+
+  // Plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-google-cloud');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify', 'copy', 'gcs']);
+
+};
---
+++ frontend/src/autowiki.js
@@ -0,0 +1,46 @@
+/*global: Pen*/
+// config
+var options = {
+  // toolbar: document.getElementById('custom-toolbar'),
+  editor: document.querySelector('[data-toggle="pen"]'),
+  debug: true,
+  list: [
+    'insertimage', 'blockquote', 'h2', 'h3', 'p', 'code', 'insertorderedlist', 'insertunorderedlist', 'inserthorizontalrule',
+    'indent', 'outdent', 'bold', 'italic', 'underline', 'createlink'
+  ]
+};
+
+// create editor
+var pen = window.pen = new Pen(options);
+
+pen.focus();
+
+// toggle editor mode
+document.querySelector('#mode').addEventListener('click', function () {
+  if (this.classList.contains('disabled')) {
+    this.classList.remove('disabled');
+    pen.rebuild();
+  } else {
+    this.classList.add('disabled');
+    pen.destroy();
+  }
+});
+
+// export content as markdown
+document.querySelector('#tomd').addEventListener('click', function () {
+  var text = pen.toMd();
+  document.body.innerHTML = '<a href="javascript:location.reload()">&larr;back to editor</a><br><br><pre>' + text + '</pre>';
+});
+
+// toggle editor mode
+document.querySelector('#hinted').addEventListener('click', function () {
+  var pen = document.querySelector('.pen');
+
+  if (pen.classList.contains('hinted')) {
+    pen.classList.remove('hinted');
+    this.classList.add('disabled');
+  } else {
+    pen.classList.add('hinted');
+    this.classList.remove('disabled');
+  }
+});
